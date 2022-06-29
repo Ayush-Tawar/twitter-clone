a73b9707a04dc09fd58e7d7e1b455f777d7f4187
Ayush-Tawar <103302142+Ayush-Tawar@users.noreply.github.com>
2022-06-29 17:07:37 +0530

    changes made

--- src/Feed.js
+++ src/Feed.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from 'react'
 import './Feed.css'
 import Post from './Post'
 import Tweetbox from './Tweetbox'
-import { db, getCities } from './Firebase'
-import FlipMove from "react-flip-move";
+// import { db, getCities } from './Firebase'
+// import FlipMove from "react-flip-move";
 
 function Feed() {
-  const [posts, setPosts] = useState([]);
+  // const [posts, setPosts] = useState([]);  
 
-  useEffect(() => {
-    getCities(db).then(function(result) {
-      setPosts(result)
-    })
+  // useEffect(() => {
+  //   getCities(db).then(function(result) {
+  //     setPosts(result)
+  //   })
 
-  }, []);
+  // }, []);
 
   return (
     <div className="feed">
@@ -21,10 +21,15 @@ function Feed() {
       <div className="feed__header">
         <h2>Home</h2>
       </div>
-
+      {/* Tweetbox */}
       <Tweetbox />  
-
-      <FlipMove>
+       
+       {/* Post */}
+       <Post/>
+       <Post/>
+       <Post/>
+       <Post/>
+      {/* <FlipMove>
         {posts.map((post) => (
           <Post
             key={post.text}
@@ -36,7 +41,7 @@ function Feed() {
             image={post.image}
           />
         ))}
-      </FlipMove>
+      </FlipMove> */}
 
     </div>
   )
